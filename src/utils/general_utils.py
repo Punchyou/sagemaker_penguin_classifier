f91b8d19393c414b134937c15521b3f02abcc17f
Maria Pantsiou <maria.th.pantsiou@gmail.com>
2024-12-11 22:03:00 +0200

    Update gitignore

--- src/utils/general_utils.py
+++ src/utils/general_utils.py
@@ -2,40 +2,6 @@ from pathlib import Path
 import pandas as pd
 
 
-def get_root_dir() -> Path:
-    """
-    Returns the root directory of the project.
-
-    Returns
-    -------
-    Path
-        The root directory of the project.
-    """
-    if "__file__" in globals():
-        return Path(__file__).resolve().parent.parent
-    else:
-        return Path.cwd()
-
-
-def get_data_from_file(data_filepath) -> pd.DataFrame:
-    """
-    Reads the data from the specified CSV file.
-
-    Parameters
-    ----------
-    data_filepath : str
-        The relative path to the CSV file.
-
-    Returns
-    -------
-    pd.DataFrame
-        The data read from the CSV file.
-    """
-    root = get_root_dir()
-    path = root / data_filepath
-    return pd.read_csv(path)
-
-
 def read_data_from_input_csv_files(base_directory):
     """
     This function reads every CSV file available and concatenates
